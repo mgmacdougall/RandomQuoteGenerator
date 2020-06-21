66f2682844931401ba0826e45f5f53d67ef43431
mgmacdougall <MacD>
2020-06-21 19:04:48 -0400

    Add of debug log, random number, getRandomQuote stubs

--- js/script.js
+++ js/script.js
@@ -3,31 +3,30 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// For assistance:
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+const debugLog = (data) => console.log(data); // debugging only, remove when done
 
-
+/***
+ * `quotes` array
+ ***/
+const quotes = [{ quote: 'test' }, { quote: 'test1' }, { quote: 'test2' }, { quote: 'test3' }, { quote: 'test4' }];
 
 /***
  * `getRandomQuote` function
-***/
-
-
+ ***/
+const randomNumber = () => Math.floor(Math.random() * quotes.length); // not using the +1 syntax b/c using the floor()
+const getRandomQuote = () => quotes[randomNumber()];
 
 /***
  * `printQuote` function
-***/
-
-
+ ***/
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
+ ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document.getElementById('load-quote').addEventListener('click', printQuote, false);
